c27369512adc6064f46bf6347b13ad382ec31681
Rezwan Chowdhury <tipu.diu.cse@gmail.com>
2024-09-13 18:00:52 +0600

    initial commit
    initial commit

---
+++ Find-BiometricType/Find-BiometricType/Find_BiometricTypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_BiometricTypeApp.swift
+//  Find-BiometricType
+//
+//  Created by Rezwan Chowdhury on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Find_BiometricTypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
